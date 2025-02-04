2280cfe344804d6cac89e1aea5a521dee32d2e16
Gabriel Galvao <gabriel.mgalvao38@gmail.com>
2025-02-04 10:51:27 -0800

    Fixed date layout to display the day of the month, thanks to Rafaela

--- main.js
+++ main.js
@@ -115,8 +115,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
     let flatDateAvailable = document.createElement("p");
     let timeStamp = Date.parse(flat.dateAvailable);
     let date = new Date(timeStamp);
+    console.log(date);
     let dateString = 
-      `${date.getMonth().toString().padStart(2, '0')}/${date.getDay().toString().padStart(2, '0')}/${date.getFullYear()}`;
+      `${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}/${date.getFullYear()}`;
     flatDateAvailable.textContent = dateString;
     flatDateAvailableDiv.appendChild(flatDateAvailable);
 
