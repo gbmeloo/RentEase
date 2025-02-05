cd8303bc6f03f6d9eb1695ba06649b68ff2083dc
Gabriel Galvao <gabriel.mgalvao38@gmail.com>
2025-02-05 12:51:07 -0800

    Added device responsiveness to login and register pages

--- Pages/Register/script.js
+++ Pages/Register/script.js
@@ -19,7 +19,22 @@ header.render();
 
 document.getElementById("register-form").addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("submit");
+
+  const existingEmailError = document.getElementById("emaiErrMsg");
+  if (existingEmailError) {
+    existingEmailError.remove();
+  }
+  
+  const existingPasswordError = document.getElementById("passwordErrorMessage");
+  if (existingPasswordError) {
+    existingPasswordError.remove();
+  }
+
+  const existingNameErrMsg = document.getElementById("nameErrMsg");
+  if (existingNameErrMsg) {
+    existingNameErrMsg.remove();
+  }
+
 
   const email = e.target.elements.email.value;
   const name = e.target.elements.userName.value;
@@ -27,34 +42,90 @@ document.getElementById("register-form").addEventListener("submit", (e) => {
   const password = e.target.elements.password.value;
   const confirmPassword = e.target.elements.confirmPassword.value;
 
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const regexPassword = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
+  const regexName = /^[A-Za-z]{2,}$/;
+
+
   if (!name) {
-    console.log("Name field can't be empty");
-    return 0;
+    const nameErrMsg = document.createElement("p");
+    nameErrMsg.textContent = "Name can't be empty";
+    nameErrMsg.id = "nameErrMsg";
+    nameErrMsg.style.color = "red";
+    nameErrMsg.style.fontSize = "12px";
+    const nameDiv = document.getElementsByClassName("userName")[0];
+    nameDiv.appendChild(nameErrMsg);
+    return;
+  } else if (!regexName.test(name)){
+    const nameErrMsg = document.createElement("p");
+    nameErrMsg.textContent = "Type a valid name";
+    nameErrMsg.id = "nameErrMsg";
+    nameErrMsg.style.color = "red";
+    nameErrMsg.style.fontSize = "12px";
+    const nameDiv = document.getElementsByClassName("userName")[0];
+    nameDiv.appendChild(nameErrMsg);
+    return;
   } else if (!lastName) {
-    console.log("Last name field can't be empty");
-    return 0;
+    const nameErrMsg = document.createElement("p");
+    nameErrMsg.textContent = "Last name can't be empty";
+    nameErrMsg.id = "nameErrMsg";
+    nameErrMsg.style.color = "red";
+    nameErrMsg.style.fontSize = "12px";
+    const lastNameDiv = document.getElementsByClassName("userLastName")[0];
+    lastNameDiv.appendChild(nameErrMsg);
+    return;
+  } else if (!regexName.test(lastName)){
+    const nameErrMsg = document.createElement("p");
+    nameErrMsg.textContent = "Type a valid last name";
+    nameErrMsg.id = "nameErrMsg";
+    nameErrMsg.style.color = "red";
+    nameErrMsg.style.fontSize = "12px";
+    const lastNameDiv = document.getElementsByClassName("userLastName")[0];
+    lastNameDiv.appendChild(nameErrMsg);
+    return;
   } else if (localStorage.getItem(email) || !email) {
-    const emailExistsErrMsg = document.createElement("p");
-    emailExistsErrMsg.textContent = !email ? "Email can't be empty" : "Email already in use";
-    emailExistsErrMsg.id = "emailExistsErrMsg";
-    emailExistsErrMsg.style.color = "red";
-    emailExistsErrMsg.style.fontSize = "12px";
+    const emailErrMsg = document.createElement("p");
+    emailErrMsg.textContent = localStorage.getItem(email) ? "Email address already registered" : "";
+    emailErrMsg.id = "emailErrMsg";
+    emailErrMsg.style.color = "red";
+    emailErrMsg.style.fontSize = "12px";
     const emailDiv = document.getElementsByClassName("email")[0];
-    emailDiv.appendChild(emailExistsErrMsg);
-    return 0;
-  } else if (!password || password !== confirmPassword) {
-    const pswdMatchErrMsg = document.createElement("p");
-    pswdMatchErrMsg.textContent = "Passwords must match";
-    pswdMatchErrMsg.id = "passwordMatchErrorMessage";
-    pswdMatchErrMsg.style.color = "red";
-    pswdMatchErrMsg.style.fontSize = "12px";
+    emailDiv.appendChild(emailErrMsg);
+    return;
+  } else if (!regexEmail.test(email)) {
+    const emailErrMsg = document.createElement("p");
+    emailErrMsg.textContent = "Type a valid email address";
+    emailErrMsg.id = "emailErrMsg";
+    emailErrMsg.style.color = "red";
+    emailErrMsg.style.fontSize = "12px";
+    const emailDiv = document.getElementsByClassName("email")[0];
+    emailDiv.appendChild(emailErrMsg);
+    return;
+  } else if (!regexPassword.test(password)) {
+    const pswdErrMsg = document.createElement("p");
+    pswdErrMsg.textContent = "Password must have at least 8 characters and 1 number";
+    pswdErrMsg.id = "passwordErrorMessage";
+    pswdErrMsg.style.color = "red";
+    pswdErrMsg.style.fontSize = "12px";
+    const confirmPasswordDiv =
+      document.getElementsByClassName("password")[0];
+    confirmPasswordDiv.appendChild(pswdErrMsg);
+    return;
+  } else if (password !== confirmPassword) {
+    const pswdErrMsg = document.createElement("p");
+    pswdErrMsg.textContent = "Passwords must match";
+    pswdErrMsg.id = "passwordErrorMessage";
+    pswdErrMsg.style.color = "red";
+    pswdErrMsg.style.fontSize = "12px";
     const confirmPasswordDiv =
       document.getElementsByClassName("confirmPassword")[0];
-    confirmPasswordDiv.appendChild(pswdMatchErrMsg);
-    return 0;
-  } else {
+    confirmPasswordDiv.appendChild(pswdErrMsg);
+    return;
+  } 
+  else {
     let storage = new UserStorage(email, name, lastName, password);
     storage.save();
     window.location.href("./login");
+    return;
   }
 });
