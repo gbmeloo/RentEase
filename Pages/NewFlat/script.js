618c8be58cb15d36b95e092995fb4337becffb2f
Gabriel Galvao <gabriel.mgalvao38@gmail.com>
2025-02-07 03:26:11 -0800

    Fixed redirection when publishing a new flat

--- Pages/NewFlat/script.js
+++ Pages/NewFlat/script.js
@@ -38,6 +38,6 @@ document.addEventListener("DOMContentLoaded", (e) => {
     const newFlat = new Flat(city, streetName, streetNumber, areaSize, hasAC, yearBuilt, rentPrice, dateAvailable);
     newFlat.save();
 
-    window.location.href = "/index.html";
+    window.location.href = "./index.html";
   });
 });
